7630730852145e8afa6c9414a524fcc3a3d48213
Tekko2000 <ot1977@gmx.net>
2021-02-09 20:57:16 +0100

    JS DOM lvl 1.8

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+// AUFGABENSTELLUNG 1
+
+let x = 20;
+let y = 30;
+let z = 10;
+
+console.log(Number(x) + Number(y));
+
+console.log(Number(x) - Number(y));
+
+console.log(Number(x) * Number(y));
+
+console.log(Number(x) / Number(y));
+
+let resultOne = (Number(x) * Number(y) / Number(z));
+
+console.log(resultOne);
+
+// AUFGABENSTELLUNG 2
+
+let a = 15;
+let b = 9;
+let c = 20;
+
+console.log(a % b);
+
+let resultTwo = (a + b) * c;
+
+console.log(resultTwo);
+
+increment = ++a
+
+console.log(increment);
+
+decrement = --b
+
+console.log(decrement);
+
+resultThree = (b - a);
+
+console.log(resultThree);
+
+console.log(resultOne / resultTwo);
+
+console.log(resultOne % resultTwo);
+
+
+
